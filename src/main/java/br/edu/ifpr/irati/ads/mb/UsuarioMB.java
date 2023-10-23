c0ff160493f602c7b55af3e55cb2d17bc4b0a7f1
CaioRWendrechoski <101076768+CaioRWendrechoski@users.noreply.github.com>
2023-10-23 08:49:55 -0300

    .

--- src/main/java/br/edu/ifpr/irati/ads/mb/UsuarioMB.java
+++ src/main/java/br/edu/ifpr/irati/ads/mb/UsuarioMB.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 import org.hibernate.Session;
 
 /**
@@ -21,7 +22,8 @@ import org.hibernate.Session;
  * @author Caio
  */
 @ManagedBean
-@RequestScoped
+//@RequestScoped
+@ViewScoped
 public class UsuarioMB implements Serializable{
     private List<Usuario> usuarios;
     private Usuario usuario;
