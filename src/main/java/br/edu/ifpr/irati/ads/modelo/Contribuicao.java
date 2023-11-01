7e1a828f4db59401f6047b384222abb60a05f306
CaioRWendrechoski <101076768+CaioRWendrechoski@users.noreply.github.com>
2023-11-01 19:43:37 -0300

    criacao classe evento

--- src/main/java/br/edu/ifpr/irati/ads/modelo/Contribuicao.java
+++ src/main/java/br/edu/ifpr/irati/ads/modelo/Contribuicao.java
@@ -4,10 +4,121 @@
  */
 package br.edu.ifpr.irati.ads.modelo;
 
+import java.util.Date;
+
 /**
  *
  * @author Caio
  */
 public class Contribuicao {
+    private int id;
+    private Usuario usuario;
+    private double valor;
+    private Date data;
+    private String formaContribuicao;
+    private String tipo;
+
+    public Contribuicao() {
+        this.id = 0;
+        this.usuario = new Usuario();
+        this.valor = 0;
+        this.data = new Date();
+        this.formaContribuicao = "";
+        this.tipo = "";
+    }
+
+    public Contribuicao(int id, Usuario usuario, double valor, Date data, String formaContribuicao, String tipo) {
+        this.id = id;
+        this.usuario = usuario;
+        this.valor = valor;
+        this.data = data;
+        this.formaContribuicao = formaContribuicao;
+        this.tipo = tipo;
+    }
+
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the data
+     */
+    public Date getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the formaContribuicao
+     */
+    public String getFormaContribuicao() {
+        return formaContribuicao;
+    }
+
+    /**
+     * @param formaContribuicao the formaContribuicao to set
+     */
+    public void setFormaContribuicao(String formaContribuicao) {
+        this.formaContribuicao = formaContribuicao;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
     
 }
---
+++ src/main/java/br/edu/ifpr/irati/ads/modelo/Evento.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifpr.irati.ads.modelo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author Caio
+ */
+public class Evento {
+    private int id;
+    private Date data;
+    private String descricao;
+    private int quantidadeFrequentantes;
+    private List<Usuario> usuarios;
+    private Usuario freqTrabalhadores;
+
+    public Evento() {
+        this.id = 0;
+        this.data = new Date();
+        this.descricao = "";
+        this.quantidadeFrequentantes = 0;
+        this.usuarios = new ArrayList<>();
+        this.freqTrabalhadores = new Usuario();
+    }
+
+    public Evento(int id, Date data, String descricao, int quantidadeFrequentantes, List<Usuario> usuarios, Usuario freqTrabalhadores) {
+        this.id = id;
+        this.data = data;
+        this.descricao = descricao;
+        this.quantidadeFrequentantes = quantidadeFrequentantes;
+        this.usuarios = usuarios;
+        this.freqTrabalhadores = freqTrabalhadores;
+    }
+
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the data
+     */
+    public Date getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the quantidadeFrequentantes
+     */
+    public int getQuantidadeFrequentantes() {
+        return quantidadeFrequentantes;
+    }
+
+    /**
+     * @param quantidadeFrequentantes the quantidadeFrequentantes to set
+     */
+    public void setQuantidadeFrequentantes(int quantidadeFrequentantes) {
+        this.quantidadeFrequentantes = quantidadeFrequentantes;
+    }
+
+    /**
+     * @return the usuarios
+     */
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    /**
+     * @param usuarios the usuarios to set
+     */
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    /**
+     * @return the freqTrabalhadores
+     */
+    public Usuario getFreqTrabalhadores() {
+        return freqTrabalhadores;
+    }
+
+    /**
+     * @param freqTrabalhadores the freqTrabalhadores to set
+     */
+    public void setFreqTrabalhadores(Usuario freqTrabalhadores) {
+        this.freqTrabalhadores = freqTrabalhadores;
+    }
+    
+    
+}
