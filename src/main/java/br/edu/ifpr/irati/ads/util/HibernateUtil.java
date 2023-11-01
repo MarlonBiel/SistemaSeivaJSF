4fefe3c8b975b27ad7dae1c9e8e53538bec6ee5a
CaioRWendrechoski <101076768+CaioRWendrechoski@users.noreply.github.com>
2023-11-01 19:09:05 -0300

    ok

--- src/main/java/br/edu/ifpr/irati/ads/util/HibernateUtil.java
+++ src/main/java/br/edu/ifpr/irati/ads/util/HibernateUtil.java
@@ -21,8 +21,7 @@ public class HibernateUtil {
             Map<String, Object> settings = new HashMap<>();
             settings.put("connection.driver_class", "com.mysql.cj.jdbc.Driver");
             settings.put("dialect", "org.hibernate.dialect.MySQLDialect");
-            settings.put("hibernate.connection.url",
-                    "jdbc:mysql://localhost:3306/jdbc:mysql://localhost:3306/sistemaseiva?createDatabaseIfNotExist=true&useUnicode=yes&characterEncoding=UTF-8");
+            settings.put("hibernate.connection.url","jdbc:mysql://localhost:3306/sistemaseiva?createDatabaseIfNotExist=true&useUnicode=yes&characterEncoding=UTF-8");            
             settings.put("hibernate.connection.username", "root");
             settings.put("hibernate.hbm2ddl.auto", "update");
             settings.put("hibernate.connection.password", "");
