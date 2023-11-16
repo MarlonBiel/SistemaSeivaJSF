1b308f6c518ec8cdb41bcf0d51283b437d3540af
CaioRWendrechoski <101076768+CaioRWendrechoski@users.noreply.github.com>
2023-11-16 19:53:01 -0300

    venda.xhtml

--- src/main/java/br/edu/ifpr/irati/ads/modelo/Venda.java
+++ src/main/java/br/edu/ifpr/irati/ads/modelo/Venda.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
 import java.io.Serializable;
 import java.util.ArrayList;
