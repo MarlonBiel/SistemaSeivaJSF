778375b653d970af11702004f610ebee64470cb8
MarlonBiel <ruanmarlon286@gmail.com>
2024-03-13 11:17:52 -0300

    Ajustes finais

--- src/main/java/br/edu/ifpr/irati/ads/mb/MensalidadeMB.java
+++ src/main/java/br/edu/ifpr/irati/ads/mb/MensalidadeMB.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.edu.ifpr.irati.ads.mb;
 
 import br.edu.ifpr.irati.ads.dao.Dao;
@@ -9,22 +5,21 @@ import br.edu.ifpr.irati.ads.dao.GenericDAO;
 import br.edu.ifpr.irati.ads.exception.PersistenceException;
 import br.edu.ifpr.irati.ads.modelo.Mensalidade;
 import br.edu.ifpr.irati.ads.modelo.Mes;
+import br.edu.ifpr.irati.ads.modelo.Transacao;
 import br.edu.ifpr.irati.ads.modelo.Usuario;
 import br.edu.ifpr.irati.ads.util.HibernateUtil;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import org.hibernate.Session;
 
-/**
- *
- * @author Caio
- */
 @ManagedBean
 @ViewScoped
-public class MensalidadeMB implements Serializable{
+public class MensalidadeMB implements Serializable {
+
     private Mensalidade mensalidade;
     private List<Mensalidade> mensalidades;
     private List<Usuario> usuarios;
@@ -35,10 +30,15 @@ public class MensalidadeMB implements Serializable{
     private Mes mes;
     private List<Mes> meses;
     private Dao<Mes> mesDAO;
-    public MensalidadeMB() throws PersistenceException{
+    private Transacao transacao = new Transacao();
+    private List<Transacao> transacoes;
+    private Dao<Transacao> transacaoDAO;
+    private Date data;
+
+    public MensalidadeMB() throws PersistenceException {
         try {
             mes = new Mes();
-            usuario=new Usuario();
+            usuario = new Usuario();
             mensalidade = new Mensalidade();
             Session session = HibernateUtil.getSessionFactory().openSession();
             mensalidadeDAO = new GenericDAO<>(Mensalidade.class, session);
@@ -54,11 +54,12 @@ public class MensalidadeMB implements Serializable{
             ex.printStackTrace();
         }
     }
+
     public void salvar() {
         try {
             Session session = HibernateUtil.getSessionFactory().openSession();
             mensalidadeDAO = new GenericDAO<>(Mensalidade.class, session);
-
+            cadastrarTransacao();
             if (inserir) {
                 //executar o método inserir do DAO
                 mensalidadeDAO.salvar(mensalidade);
@@ -75,9 +76,11 @@ public class MensalidadeMB implements Serializable{
             ex.printStackTrace();
         }
     }
+
     public void limparTela() {
         setMensalidade(new Mensalidade());
     }
+
     public void botaoExcluir(Mensalidade mensalidade) {
         try {
             Session session = HibernateUtil.getSessionFactory().openSession();
@@ -92,96 +95,71 @@ public class MensalidadeMB implements Serializable{
         }
 
     }
+
     public String botaoAlterar(Mensalidade mensalidade) {
         System.out.println(mensalidade.getId());
         this.mensalidade = new Mensalidade(mensalidade.getId(), mensalidade.getUsuario(), mensalidade.getMes(), mensalidade.getAno(), mensalidade.getValor());
         inserir = false;
         return "-";
     }
-    /**
-     * @return the mensalidade
-     */
+
+    public void cadastrarTransacao() throws PersistenceException {
+        data = new Date();
+        char tipo = 'C';
+        transacao = new Transacao(0, data, mensalidade.getUsuario().getNome(), tipo, mensalidade.getValor());
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        transacaoDAO = new GenericDAO<>(Transacao.class, session);
+        transacaoDAO.salvar(transacao);
+        session.close();
+        transacao = new Transacao();
+    }
+
     public Mensalidade getMensalidade() {
         return mensalidade;
     }
 
-    /**
-     * @param mensalidade the mensalidade to set
-     */
     public void setMensalidade(Mensalidade mensalidade) {
         this.mensalidade = mensalidade;
     }
-    
-    /**
-     * @return the mensalidades
-     */
+
     public List<Mensalidade> getMensalidades() {
         return mensalidades;
     }
 
-    /**
-     * @param mensalidades the mensalidades to set
-     */
     public void setMensalidades(List<Mensalidade> mensalidades) {
         this.mensalidades = mensalidades;
     }
-    
-    /**
-     * @return the usuarios
-     */
+
     public List<Usuario> getUsuarios() {
         return usuarios;
     }
 
-    /**
-     * @param usuarios the usuarios to set
-     */
     public void setUsuarios(List<Usuario> usuarios) {
         this.usuarios = usuarios;
     }
-    /**
-     * @return the meses
-     */
+
     public List<Mes> getMeses() {
         return meses;
     }
 
-    /**
-     * @param meses the meses to set
-     */
     public void setMeses(List<Mes> meses) {
         this.meses = meses;
     }
 
-    /**
-     * @return the mes
-     */
     public Mes getMes() {
         return mes;
     }
 
-    /**
-     * @param mes the mes to set
-     */
     public void setMes(Mes mes) {
         this.mes = mes;
     }
 
-    /**
-     * @return the usuario
-     */
     public Usuario getUsuario() {
         return usuario;
     }
 
-    /**
-     * @param usuario the usuario to set
-     */
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
     }
 
-   
-    
-    
 }
